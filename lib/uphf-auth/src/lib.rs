be8418e2f4d5f11730e198345f13d09a7475d163
Rémi Ait-Younes <remi@ait-younes.fr>
2024-09-20 20:34:13 +0200

    feat(uphf-auth): add get_edt_jsession_id fn
    it gets a jsession id by following a redirect containing a ticket given
    by the cas

--- lib/uphf-auth/src/lib.rs
+++ lib/uphf-auth/src/lib.rs
@@ -13,14 +13,17 @@ pub async fn get_new_cas_execution_value() -> String {
     el.attr("value").unwrap().to_owned()
 }
 
-pub async fn get_cas_tgc_cookie(username: &str, password: &str, execution_value: &str) -> String {
-    // TODO: make it global
+fn get_reqwest_client() -> reqwest::Client {
     static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
-    let client = reqwest::Client::builder()
+    reqwest::Client::builder()
         .user_agent(APP_USER_AGENT)
         .build()
-        .unwrap();
+        .unwrap()
+}
+
+pub async fn get_cas_tgc_cookie(username: &str, password: &str, execution_value: &str) -> String {
+    let client = get_reqwest_client();
     let form_params = [
         ("username", username),
         ("password", password),
@@ -36,4 +39,17 @@ pub async fn get_cas_tgc_cookie(username: &str, password: &str, execution_value:
         .unwrap();
     let cookie = response.cookies().find(|cookie| cookie.name() == "TGC");
     cookie.unwrap().value().to_owned()
+}
+
+pub async fn get_edt_jsession_id(tgc_cookie: &str) -> String {
+    let client = get_reqwest_client();
+    let response = client
+        .get("https://cas.uphf.fr/cas/login?service=https://vtmob.uphf.fr/esup-vtclient-up4/stylesheets/desktop/welcome.xhtml")
+        .header("Cookie", format!("TGC={}", tgc_cookie))
+        .send()
+        .await
+        .unwrap();
+    // TODO: maybe try to get JSESSIONID from the header in a previous redirect?
+    let url = response.url().to_string();
+    url.split("=").last().unwrap().to_owned()
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ async fn main() {
     let execution_value = dbg!(uphf_auth::get_new_cas_execution_value().await);
 
     let cookie = dbg!(uphf_auth::get_cas_tgc_cookie("", "", &execution_value).await);
+
+    let jsession = dbg!(uphf_auth::get_edt_jsession_id(&cookie).await);
 }
